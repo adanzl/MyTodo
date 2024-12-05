b2011bed3a9051adc412c56ac81f71343b7ad649
zhaolin09 <zhaolin09@58.com>
2024-12-05 13:39:00 +0800

    发布

--- server/main.py
+++ server/main.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+# . .venv/bin/activate
 if __name__ == '__main__':
     app.run(debug=True, port=8888)  #开始运行flask应用程序，以调试模式运行
