e8c35f57f438ecececd8ba38af0b9b2c7875a9d3
zhaolin09 <zhaolin09@58.com>
2024-12-05 16:31:55 +0800

    !

--- server/main.py
+++ server/main.py
@@ -7,5 +7,8 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 # . .venv/bin/activate
+# sudo vim /etc/systemd/system/MyTodo.service
+# sudo vim  /etc/caddy/Caddyfile
+# caddy reload --config=/etc/caddy/Caddyfile
 if __name__ == '__main__':
     app.run(debug=True, port=8888)  #开始运行flask应用程序，以调试模式运行
