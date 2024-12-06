120a7a5d22a7026b4447bbaf2d8ae4b186ecd5fe
zhaolin09 <zhaolin09@58.com>
2024-12-06 17:24:05 +0800

    add server code

---
+++ server/db_mgr.py
@@ -0,0 +1,61 @@
+import logging
+import sqlite3
+import traceback
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+DB_NAME = "data.db"
+TABLE_SAVE = "t_user_save"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    create_t_user_save_sql = f'''
+    CREATE TABLE IF NOT EXISTS {TABLE_SAVE} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_name TEXT NOT NULL,
+        data TEXT
+    );
+    '''
+    cur.execute(create_t_user_save_sql)
+    conn.commit()
+    cur.close()
+
+
+def get_save(id):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    try:
+        cur.execute(f"SELECT * FROM {TABLE_SAVE} WHERE id=?", (id))
+        result = cur.fetchone()
+        if result:
+            return result[2]
+        else:
+            return "{}"
+    except Exception as e:
+        log.error(e)
+        traceback.print_exc()
+        return "{}"
+    finally:
+        cur.close()
+
+
+def set_save(id, user_name, data):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            f"""
+            INSERT INTO {TABLE_SAVE} (id, user_name,data) VALUES (?,?,?)
+            ON CONFLICT(id) DO UPDATE SET data=?;
+            """, (id, user_name, data, data))
+        conn.commit()
+    except Exception as e:
+        log.error(e)
+        traceback.print_exc()
+        return 'error'
+    finally:
+        cur.close()
+    return "ok"
--- server/main.py
+++ server/main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, json, request, jsonify
+import logging
+import db_mgr
 
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
+db_mgr.init_db()
+
+
 @app.route("/")
-def hello_world():
+def main():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/getSave", methods=['GET'])
+def get_save():
+    id = request.args.get('id')
+    log.info("===== [Get Save] " + id)
+    return db_mgr.get_save(id)
+
+
+@app.route("/setSave", methods=['POST'])
+def set_save():
+    args = request.get_json()
+    log.info("===== [Set Save] " + json.dumps(args))
+    id = args.get('id')
+    user_name = args.get('user_name')
+    data = json.dumps(args.get('data'))
+    return db_mgr.set_save(id, user_name, data)
+
 
 # . .venv/bin/activate
 # sudo vim /etc/systemd/system/MyTodo.service
