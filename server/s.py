32d8c09d80380f0bc9ade038d8685fcd507c8240
zhaolin09 <zhaolin09@58.com>
2025-05-08 19:03:59 +0800

    opt

--- server/s.py
+++ server/s.py
@@ -1,38 +1,33 @@
-import core.ai.ai_mgr as ai_mgr
-import core.db.db_mgr as db_mgr
-from app import app, socketio
-from core.chat.chat_mgr import ChatMgr
+from flask import Response
 from core.log_config import root_logger
 
 log = root_logger()
 
-chat_mgr = ChatMgr()
-db_mgr.init()
-ai_mgr.init()
+
+def default_app(env, resp):
+    return Response("Not Found", status=404)
 
 
-@app.route("/")
-def main():
-    return "<p>Hello, World!</p>"
+def null_application(environ, start_response):
+    start_response("404 NOT FOUND", [("Content-Type", "text/plain")])
+    yield b"NOT FOUND"
 
 
-# . .venv/bin/activate
 if __name__ == '__main__':
-    # app.run(debug=True, port=8888)
     from werkzeug.serving import run_simple
     import os
     from werkzeug.middleware.dispatcher import DispatcherMiddleware
     from werkzeug.middleware.shared_data import SharedDataMiddleware
     try:
         base_dir = os.path.dirname(os.path.abspath(__file__))  # 假设代码文件在项目根目录
-        static_app = SharedDataMiddleware(None, {'/web': os.path.join(base_dir, 'static')})
+        print(os.path.join(base_dir, 'static'))
+        static_app = SharedDataMiddleware(null_application, {'/': os.path.join(base_dir, 'static')})
+
         application = DispatcherMiddleware(
-            None,  # 主应用（此处为空）
+            None,
             {
-                '/api': app,
                 '/web': static_app,  # 静态文件挂载到 /web
             })
         run_simple('localhost', 8000, application, use_reloader=True, use_debugger=True, reloader_type='stat')
-        # socketio.run(app, debug=True, port=8000)
     except Exception as e:
         log.error(e)
