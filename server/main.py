4097a306198cf3064d8f62e41ba8a0308d4cba42
zhaolin09 <zhaolin09@58.com>
2025-05-09 18:49:44 +0800

    ui

--- server/main.py
+++ server/main.py
@@ -16,21 +16,35 @@ def main():
     return "<p>Hello, World!</p>"
 
 
-# . .venv/bin/activate
+def null_application(environ, start_response):
+    start_response("404 NOT FOUND", [("Content-Type", "text/plain")])
+    yield b"NOT FOUND"
+
+
 if __name__ == '__main__':
-    # app.run(debug=True, port=8888)
     from werkzeug.serving import run_simple
     from werkzeug.middleware.dispatcher import DispatcherMiddleware
     from werkzeug.middleware.shared_data import SharedDataMiddleware
+    import os
     try:
-        static_app = SharedDataMiddleware(None, {'/': 'static'})
+        base_dir = os.path.dirname(os.path.abspath(__file__))  # 假设代码文件在项目根目录
+        static_app = SharedDataMiddleware(null_application, {'/': 'static'})
         application = DispatcherMiddleware(
-            None,  # 主应用（此处为空）
+            null_application,  # 主应用（此处为空）
             {
                 '/api': app,
                 '/web': static_app,  # 静态文件挂载到 /web
             })
-        run_simple('localhost', 8000, application, use_reloader=True, use_debugger=True, reloader_type='stat')
-        # socketio.run(app, debug=True, port=8000)
+        js_files = [os.path.join('static', f) for f in os.listdir('static') if f.endswith('.js') or f.endswith('.html')]
+        run_simple(
+            'localhost',
+            8000,
+            application,
+            use_reloader=True,
+            use_debugger=True,
+            threaded=False,  # 禁用多线程
+            processes=1,  # 禁用多进程
+            reloader_type='stat',
+            extra_files=js_files)
     except Exception as e:
         log.error(e)
--- server/static/view/lottery.js
+++ server/static/view/lottery.js
@@ -5,7 +5,10 @@ const { ElMessage } = window.ElementPlus;
 const Lottery = {
   setup() {
     const refData = {
+      selectedCate: ref(),
+      modifyCateModel: ref(false),
       lotteryData: ref([]),
+      lotteryCatList: ref([]),
     };
     const handleEdit = (index, row) => {
       console.log(index, row);
@@ -13,10 +16,14 @@ const Lottery = {
     const handleDelete = (index, row) => {
       console.log(index, row);
     };
+    const onCateChange = (item) => {
+      console.log(item);
+    };
     return {
       ...refData,
       handleEdit,
       handleDelete,
+      onCateChange,
     };
   },
   async mounted() {
@@ -25,7 +32,8 @@ const Lottery = {
       .then((data) => {
         const d = data.data;
         console.log(d);
-        Object.assign(this.lotteryData, d); // 浅合并
+        Object.assign(this.lotteryCatList, d); // 浅合并
+        this.lotteryCatList.unshift({ id: 0, name: "全部" });
       })
       .catch((err) => {
         console.error(err);
@@ -41,31 +49,50 @@ const Lottery = {
     increment() {},
   },
   template: `
-    <div class="m-8">
-        <el-table :data="lotteryData" style="width: 100%">
-          <el-table-column label="Img" width="80" >
-            <template #default="scope">
-              <el-avatar :src="scope.row.img" />
-            </template>
-          </el-table-column>
-          <el-table-column prop="name" label="Name" width="100" />
-          <el-table-column prop="weight" label="Weight" width="180" />
-          <el-table-column label="Operations" >
-            <template #default="scope">
-              <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
-                Edit
-              </el-button>
-              <el-button
-                size="small"
-                type="danger"
-                @click="handleDelete(scope.$index, scope.row)"
-              >
-                Delete
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+    <div class="m-4">
+      <div class="flex items-center">
+        <el-radio-group size="large" v-model="selectedCate" class="" @change="onCateChange(selectedCate)">
+          <el-radio-button v-for='item in lotteryCatList' :key='item.id' :value='item'> {{ item.name }}</el-radio-button>
+        </el-radio-group>
+        <el-button type="primary" class="ml-1" @click="modifyCateModel=true"><el-icon><Edit /></el-icon></el-button>
+        <div class="flex-1 flex items-center justify-end">
+          <el-button type="primary" class="">添加奖品</el-button>
+        </div>
+      </div>
+      <el-table :data="lotteryData" style="width: 100%">
+        <el-table-column label="Img" width="80" >
+          <template #default="scope">
+            <el-avatar :src="scope.row.img" />
+          </template>
+        </el-table-column>
+        <el-table-column prop="name" label="Name" width="100" />
+        <el-table-column prop="weight" label="Weight" width="180" />
+        <el-table-column label="Operations" >
+          <template #default="scope">
+            <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
+              Edit
+            </el-button>
+            <el-button
+              size="small"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+            >
+              Delete
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
+    <el-dialog v-model="modifyCateModel" title="类别管理" width="800" destroy-on-close>
+      <el-table :data="lotteryCatList">
+        <el-table-column property="id" label="ID" width="150" />
+        <el-table-column property="name" label="Name" width="200" />
+        <el-table-column  label="OP" />
+      </el-table>
+      <el-button class="mt-4" style="width: 100%" @click="onAddItem">
+        Add Item
+      </el-button>
+    </el-dialog>
   `,
 };
 
