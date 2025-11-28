63cf0ed0dc000f807beee2778c66ca02d35f6e05
zhaolin09 <zhaolin09@58.com>
2025-11-28 11:05:18 +0800

    opt

--- server/core/models/playlist.py
+++ server/core/models/playlist.py
@@ -137,8 +137,6 @@ class PlaylistMgr:
         success = scheduler.add_cron_job(func=cron_play_task, job_id=job_id, cron_expression=cron_expression)
         playlist_name = playlist_data.get("name", "未知播放列表")
         if success:
-            job = scheduler.get_job(job_id)
-            next_run_time = job.next_run_time.strftime("%Y-%m-%d %H:%M:%S") if job and job.next_run_time else "未知"
             log.info(f"[PlaylistMgr] 创建定时任务成功: {playlist_id}, {playlist_name}, cron: {cron_expression}")
         else:
             log.error(f"[PlaylistMgr] 创建定时任务失败: {playlist_id}, {playlist_name}, cron: {cron_expression}")
