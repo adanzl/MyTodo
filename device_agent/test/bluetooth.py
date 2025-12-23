6242a346e8c0346bd247ad15c0e0bae152b4595f
zhaolin09 <zhaolin09@58.com>
2025-12-23 18:47:18 +0800

    tt

--- device_agent/test/bluetooth.py
+++ device_agent/test/bluetooth.py
@@ -12,12 +12,21 @@ if __name__ == "__main__":
     # 测试代码
     print("Bluetooth Manager Test")
     # 测试扫描
-    devices = bluetooth_mgr.scan_devices_sync(timeout=3.0)
-    print(f"Found {len(devices)} devices")
-    for device in devices:
-        print(f"【{device['address']}】{device['name']}")
+    # devices = bluetooth_mgr.scan_devices_sync(timeout=3.0)
+    # print(f"Found {len(devices)} devices")
+    # for device in devices:
+    #     print(f"【{device['address']}】{device['name']}")
     # 测试获取系统已连接的设备
-    connected_devices = bluetooth_mgr.get_paired_devices()
-    print(f"Found {len(connected_devices)} connected devices")
-    for device in connected_devices:
-        print(f"【{device.get('address', 'N/A')}】{device.get('name', 'Unknown')}")
+    # connected_devices = bluetooth_mgr.get_connected_devices()
+    # print(f"Found {len(connected_devices)} connected devices")
+    # for device in connected_devices:
+    #     print(device)
+    device_address = "90:FB:5D:E9:F0:9B"
+    info = bluetooth_mgr.get_info(device_address)
+    print(f"{device_address} connected: {info['data']['connected']}")
+    print(bluetooth_mgr.disconnect_device_sync(device_address))
+    info = bluetooth_mgr.get_info(device_address)
+    print(f"{device_address} connected: {info['data']['connected']}")
+    # bluetooth_mgr.connect_device_sync(device_address)
+    # info = bluetooth_mgr.get_info(device_address)
+    # print(info['data'])
