c02a3470cbd249a4843a70804330fe6f9c655e1e
zhaolin09 <zhaolin09@58.com>
2026-01-23 17:16:30 +0800

    f

--- server/main.py
+++ server/main.py
@@ -3,7 +3,7 @@ from gevent import monkey
 # thread=False 表示不 patch threading 模块，这样 ThreadPoolExecutor 会使用真正的线程
 # subprocess=False 表示不 patch subprocess 模块，避免 gevent subprocess 在新线程中的 "child watchers are only available on the default loop" 错误
 # 大部分代码已使用 run_subprocess_safe（使用 os.system），不依赖 gevent subprocess patch
-monkey.patch_all(subprocess=False, thread=False, queue=False)
+monkey.patch_all(subprocess=True, thread=False, queue=False)
 
 # 在导入其他模块之前加载环境变量
 from dotenv import load_dotenv
