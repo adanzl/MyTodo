fe5375ce4bb685dab05d673619d8d272a240ab0d
leo <adanzl@163.com>
2026-07-05 22:01:56 +0800

    check full

--- server/check_schedule.py
+++ server/check_schedule.py
@@ -1,6 +1,7 @@
 import sqlite3
 c = sqlite3.connect("data.db")
-r = c.execute("SELECT id, title, start_ts, repeat FROM t_schedule WHERE id=329 OR title LIKE '%测试%'").fetchall()
-for row in r:
-    print(row)
+r = c.execute("SELECT * FROM t_schedule WHERE id=329").fetchall()
+print(r[0].keys())
+for k, v in zip(r[0].keys(), r[0]):
+    print(k, "=", v)
 c.close()
