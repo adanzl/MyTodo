6d3a7916c322314b85b2cd36a51908732c284294
leo <adanzl@163.com>
2026-07-05 16:36:50 +0800

    debug: 添加 SelectorRepeat dismiss 日志确认 prod 代码是否加载

--- src/views/page-schedule/dialogs/SelectorRepeat.vue
+++ src/views/page-schedule/dialogs/SelectorRepeat.vue
@@ -89,9 +89,11 @@ const valueRef = ref(props.value.repeat ?? 0);
 const repeatData = ref(props.value.repeatData ?? new RepeatData());
 
 const cancel = () => {
+  console.log("[SelectorRepeat] cancel dismiss");
   modal.value.$el!.dismiss();
 };
 const confirm = () => {
+  console.log("[SelectorRepeat] confirm dismiss");
   emits("update:value", valueRef.value, repeatData.value);
   modal.value.$el!.dismiss();
 };
