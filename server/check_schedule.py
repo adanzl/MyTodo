9b5f034bcf06da902ff4a7f00d4e0819f4b9a7cb
leo <adanzl@163.com>
2026-07-05 22:13:59 +0800

    chore: 清理临时脚本

--- server/check_schedule.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-c = sqlite3.connect("data.db")
-c.row_factory = sqlite3.Row
-r = c.execute("SELECT id,title,start_ts,user_id,repeat FROM t_schedule WHERE id=329").fetchone()
-print(dict(r))
-c.close()
