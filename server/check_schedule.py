6d7d81453c948ab3a73ebd60e179bd73a518fd57
leo <adanzl@163.com>
2026-07-05 22:02:23 +0800

    fix row factory

--- server/check_schedule.py
+++ server/check_schedule.py
@@ -1,7 +1,7 @@
 import sqlite3
 c = sqlite3.connect("data.db")
+c.row_factory = sqlite3.Row
 r = c.execute("SELECT * FROM t_schedule WHERE id=329").fetchall()
-print(r[0].keys())
-for k, v in zip(r[0].keys(), r[0]):
-    print(k, "=", v)
+for k in r[0].keys():
+    print(k, "=", repr(r[0][k]))
 c.close()
